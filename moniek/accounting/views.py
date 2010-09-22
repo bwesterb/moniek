a7692bc810d1a30c9d0fa91bf1689651bd34d8c6
Bas Westerbaan <bas@westerbaan.name>
2010-09-22 15:51:13 +0200

    Create home and accounts stub view
    Signed-off-by: Bas Westerbaan <bas@westerbaan.name>

--- moniek/accounting/views.py
+++ moniek/accounting/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 
 def home(request):
+	return render_to_response('accounting/home.html', {},
+			RequestContext(request))
+def accounts(request):
 	return HttpResponse("Hi")
